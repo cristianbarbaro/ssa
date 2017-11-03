f87a44126591c5ff1ade40b90645d26fc582cb25
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-11-03 16:14:37 -0300

    Seed en la base de datos

--- migrate-database.sh
+++ migrate-database.sh
@@ -3,3 +3,6 @@ set -e
 
 echo "Migrating database 'php artisan migrate --force'..."
 php artisan migrate --force
+# Carga datos en la base de datos:
+echo "Seeding database 'php artisan db:seed'"
+php artisan db:seed
