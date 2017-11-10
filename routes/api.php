bf87ce8830713f718b00737011d8848296f3548a
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-11-10 14:27:17 -0300

    se devuelven incidentes con sus objetos en api

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,7 @@ Use App\Objeto;
 // });
 
 Route::get('incidentes', function() {
-    // If the Content-Type and Accept headers are set to 'application/json',
-    // this will return a JSON structure. This will be cleaned up later.
-    return Incidente::all();
+    return Incidente::with('objetos')->get();
 });
 
 Route::get('incidentes/noAsignado', function() {
