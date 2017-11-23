b7025ffa2edcdfb0776c22b84265a46e410d81d9
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-11-23 16:57:57 -0300

    Rutas para modificar el estado de un incidente agregadas

--- routes/api.php
+++ routes/api.php
@@ -24,21 +24,54 @@ Route::get('incidentes', function() {
 });
 
 Route::get('incidentes/noAsignado', function() {
+    // retorna el primer incidente que no ha sido asignado.
     $incidente = Incidente::where('estado', 'CREADO')->first();
 
     return $incidente->load('objetos');
 });
 
 Route::get('incidentes/{id}', function($id) {
-    return Incidente::find($id);
+    // retorna el incidente con el id pedido.
+    return Incidente::findOrFail($id);
+});
+
+Route::post('incidentes/cambioEstado/recibido', function(Request $request) {
+    $incidente = Incidente::findOrFail($request->get('idIncidente'));
+    $incidente->estado = "RECIBIDO";
+    $incidente->save();
+    //$incidente->update(['estado' => "RECIBIDO"]);
+    return $incidente;
+});
+
+Route::post('incidentes/cambioEstado/seguimiento', function(Request $request) {
+    $incidente = Incidente::findOrFail($request->get('idIncidente'));
+    $incidente->estado = "SEGUIMIENTO";
+    $incidente->save();
+    return $incidente;
+});
+
+Route::post('incidentes/cambioEstado/aceptado', function(Request $request) {
+    $incidente = Incidente::findOrFail($request->get('idIncidente'));
+    $incidente->estado = "ACEPTADO";
+    $incidente->save();
+    return $incidente;
+});
+
+Route::post('incidentes/cambioEstado/rechazado', function(Request $request) {
+    $incidente = Incidente::findOrFail($request->get('idIncidente'));
+    $incidente->estado = "RECHAZADO";
+    $incidente->save();
+    return $incidente;
 });
 
 Route::post('incidentes', function(Request $request) {
+    // crea un nuevo incidente. Espera recibir un json de la siguiente forma:
     // { "user_id": 1, "descripcionIncidente": "Descripcion de incidente", "fechaIncidente": "2017/10/10" }
     return Incidente::create($request->all());
 });
 
 Route::put('incidentes/{id}', function(Request $request, $id) {
+    // actualiza datos del incidente. Espera recibir un json.
     $incidente = Incidente::findOrFail($id);
     $incidente->update($request->all());
 
@@ -46,7 +79,8 @@ Route::put('incidentes/{id}', function(Request $request, $id) {
 });
 
 Route::delete('incidentes/{id}', function($id) {
-    Incidente::find($id)->delete();
+    // borra un incidente con id recibido.
+    Incidente::findOrFail($id)->delete();
 
     return 204;
 });
